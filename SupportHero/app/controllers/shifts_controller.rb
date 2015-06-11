e63a2f8328c30854bc6eee3bebbcde04e41f0522
Calvin Wang <calvinkw1@gmail.com>
2015-06-10 17:09:47 -0700

    fixed default selection on edit shift view by changing default to selected

--- SupportHero/app/controllers/shifts_controller.rb
+++ SupportHero/app/controllers/shifts_controller.rb
@@ -1,4 +1,6 @@
 class ShiftsController < ApplicationController
+  before_action :all_users, only: [:new, :edit, :update]
+
   def index
     @shifts = Shift.all
     @user_shifts = Shift.joins(:user)
@@ -6,24 +8,22 @@ class ShiftsController < ApplicationController
 
   def new
     @shift = Shift.new
-    @names = []
-    User.all.each do |user|
-      @names << user.name
-    end
+    # @names = []
+    # User.all.each do |user|
+    #   @names << user.name
+    # end
   end
 
   def create
     dateStr = params.values[2].values[0]
     name = params.values[2].values[1]
+    user = User.where('name=?', params.values[2].values[1])
     if dateStr
       date_split = dateStr.split('/')
-      # using the Holiday gem to check for observed US holidays
       date = Date.civil(date_split[2].to_i, date_split[0].to_i, date_split[1].to_i)
-      holiday = Holidays.on(date, :us)
     end
-    if date && name && holiday
-      user = User.where('name=?', params.values[2].values[1])
-      shift = Shift.create(date: date, user_id: user[0].id, holiday: holiday, weekend: date.saturday? || date.sunday?)
+    if date && name
+      shift = Shift.create(date: date, user_id: user[0].id, holiday: date.holiday?, weekend: date.saturday? || date.sunday?)
       if shift.save
         redirect_to shifts_path
       else
@@ -37,6 +37,33 @@ class ShiftsController < ApplicationController
 
   def edit
     @shift = Shift.find params[:id]
+    @user = User.find params.values[2]
+    # @names = []
+    # User.all.each do |user|
+    #   @names << user.name
+    # end
+  end
+
+  def update
+    @shift = Shift.find params[:id]
+    user = User.where('name=?', params.values[3].values[1])
+    binding.pry
+    if params.values[3].values[1] == "" && params.values[3].values[2] == false
+      @shift.avail = false
+      @shift.save
+      binding.pry
+    else
+      @shift.user_id = user[0].id
+    end
+
+    @shift.avail = params.values[3].values[2]
+    if @shift.avail == false
+      @names - user.name
+      binding.pry
+    else
+      @shift.save
+      redirect_to shifts_path
+    end
   end
 
   private
@@ -51,5 +78,12 @@ class ShiftsController < ApplicationController
   #   params.require(:shift).permit(:date, :user_id)
   #   binding.pry
   # end
+  
+  def all_users
+    @names = []
+    User.all.each do |user|
+      @names << user.name
+    end
+  end
 
 end
